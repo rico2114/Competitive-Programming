e0d7c41fc1e07578b177ffabb9b7a4521bcef3b6
Juan Sebastian Quiceno <juan.2114@hotmail.com>
2018-04-22 00:41:12 -0500

    A skeleton file (unfinished) for Java

---
+++ UVA/Book 1/Volume 112/Skeleton.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import javafx.util.Pair;
+
+public class C366 {
+	
+	public static void main(final String args[]) {
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		final PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out));
+		
+	}
+}
